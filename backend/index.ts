5f1366e2fba42794d5066df89b4645fc845c1434
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-03-29 13:16:16 +0900

    UPDATE index.ts Apollo Server

--- backend/index.ts
+++ backend/index.ts
@@ -1,3 +1,28 @@
 import database from "./database";
+import { ApolloServer, gql, ServerInfo } from "apollo-server";
 
-console.log(database);
+const typeDefs = gql`
+  type Query {
+    teams: [Team]
+  }
+  type Team {
+    id: Int 
+    manager: String
+    office: String
+    extension_number: String
+    mascot: String
+    cleaning_duty: String
+    project: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    teams: () => database.teams
+  }
+}
+
+const server = new ApolloServer({ typeDefs, resolvers });
+server.listen().then((res: ServerInfo) => {
+  console.log(res.url);
+});
