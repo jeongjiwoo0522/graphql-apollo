be936c5370a7e11c19b7621e5adbc7fd0bd0f620
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-06-08 23:13:08 +0900

    UPDATE typeDefs type 설정

--- project/api/index.ts
+++ project/api/index.ts
@@ -1,8 +1,43 @@
 import { ApolloServer, gql } from "apollo-server";
 
 const typeDefs = gql`
+  enum PhotoCategory {
+    SELFIE
+    PORTRAIT
+    ACTION
+    LANDSCAPE
+    GRAPHIC
+  }
+
+  input PostPhotoInput {
+    name: String!
+    category: PhotoCategory=PORTRAIT
+    description: String
+  }
+
+  type User {
+    githubLogin: ID!
+    name: String
+    avatar: String
+    postedPhotos: [Photo!]!
+  }
+
+  type Photo {
+    id: ID!
+    url: String
+    name: String!
+    description: String
+    category: PhotoCategory!
+    postedBy: User!
+  }
+
   type Query {
     totalPhotos: Int!
+    allPhotos: [Photo!]!
+  }
+
+  type Mutation {
+    postPhoto(input: PostPhotoInput!): Boolean!
   }
 `;
 
