321b0983dc831acf642f874c31fc7a19f47bf73b
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-06-09 17:09:00 +0900

    CREATE gql metadata

---
+++ project/api/gql/typeDefs.ts
@@ -0,0 +1,48 @@
+import { gql } from "apollo-server-express";
+
+export const typeDefs = gql`
+  scalar DateTime
+
+  enum PhotoCategory {
+    SELFIE
+    PORTRAIT
+    ACTION
+    LANDSCAPE
+    GRAPHIC
+  }
+
+  input PostPhotoInput {
+    name: String!
+    category: PhotoCategory=PORTRAIT
+    description: String
+  }
+
+  type User {
+    githubLogin: ID!
+    name: String
+    avatar: String
+    postedPhotos: [Photo!]!
+    inPhotos: [Photo!]!
+  }
+
+  type Photo {
+    id: ID!
+    url: String
+    name: String!
+    description: String
+    category: PhotoCategory!
+    postedBy: User!
+    taggedUsers: [User!]!
+    created: DateTime!
+    addPhotos(after: DateTime): [Photo!]!
+  }
+
+  type Query {
+    totalPhotos: Int!
+    allPhotos: [Photo!]!
+  }
+
+  type Mutation {
+    postPhoto(input: PostPhotoInput!): Boolean!
+  }
+`;
--- project/api/index.ts
+++ project/api/index.ts
@@ -1,58 +1,7 @@
-import { ApolloServer, gql } from "apollo-server-express";
 import express from "express";
-
-const typeDefs = gql`
-  scalar DateTime
-
-  enum PhotoCategory {
-    SELFIE
-    PORTRAIT
-    ACTION
-    LANDSCAPE
-    GRAPHIC
-  }
-
-  input PostPhotoInput {
-    name: String!
-    category: PhotoCategory=PORTRAIT
-    description: String
-  }
-
-  type User {
-    githubLogin: ID!
-    name: String
-    avatar: String
-    postedPhotos: [Photo!]!
-    inPhotos: [Photo!]!
-  }
-
-  type Photo {
-    id: ID!
-    url: String
-    name: String!
-    description: String
-    category: PhotoCategory!
-    postedBy: User!
-    taggedUsers: [User!]!
-    created: DateTime!
-    addPhotos(after: DateTime): [Photo!]!
-  }
-
-  type Query {
-    totalPhotos: Int!
-    allPhotos: [Photo!]!
-  }
-
-  type Mutation {
-    postPhoto(input: PostPhotoInput!): Boolean!
-  }
-`;
-
-const resolvers = {
-  Query: {
-    totalPhotos: () => 42
-  }
-}
+import { ApolloServer } from "apollo-server-express";
+import { typeDefs } from "./gql/typeDefs";
+import { resolvers } from "./gql/resolvers";
 
 const app = express();
 
@@ -64,4 +13,4 @@ app.get("/", (req, res) => res.end("PhotoShare API에 오신 것을 환영합니
 
 app.listen(4000, () => {
   console.log(`GraphQL Server running @ http://localhost:4000${server.graphqlPath}`)
-})
+}); 
