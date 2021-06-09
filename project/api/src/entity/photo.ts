ee0446ba2bfbdfee13d29abb19cdefd5aaa16029
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-06-09 20:01:51 +0900

    CREATE user.ts, photo.ts entity 타입 정의

--- project/api/src/entity/photo.ts
+++ project/api/src/entity/photo.ts
@@ -1,1 +1,38 @@
-export const photos = [];
+export enum PhotoCategory {
+  SELFIE = "SELFIE",
+  PORTRAIT = "PORTRAIT",
+  ACTION = "ACTION",
+  LANDSCAPE = "LANDSCAPE",
+  GRAPHIC = "GRAPHIC",
+}
+
+export interface Photo {
+  id: number;
+  name: string;
+  description?: string;
+  category: PhotoCategory;
+  githubUser: string;
+}
+
+export const photos: Photo[] = [
+  {
+    id: 1,
+    name: "Dropping the Heart Chute",
+    description: "The heart chute is one of my favorite chutes",
+    category: PhotoCategory.ACTION,
+    githubUser: "gPlake"
+  }, 
+  {
+    id: 2,
+    name: "Enjoying the sunshine",
+    category: PhotoCategory.SELFIE,
+    githubUser: "sSchmidt"
+  },
+  {
+    id: 3,
+    name: "Gunbarrel 20",
+    description: "25 laps on gunbarrel today",
+    category: PhotoCategory.LANDSCAPE,
+    githubUser: "sSchmidt"
+  },
+];
---
+++ project/api/src/entity/user.ts
@@ -0,0 +1,19 @@
+export interface User { 
+  githubLogin: string;
+  name: string;
+}
+
+export const users: User[] = [
+  {
+    githubLogin: "mHattrup",
+    name: "Mike Hattrup",
+  },
+  {
+    githubLogin: "gPlake",
+    name: "Glen Plake",
+  },
+  {
+    githubLogin: "sSchmidt",
+    name: "Scot Schmidt",
+  },
+];
